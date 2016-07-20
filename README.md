86e6967e4a6e84a71f059f20ffaed47702d76cfb
chefkoch009 <info@punktnetzwerk.net>
2016-07-20 23:37:38 +0200

    (chefkoch009) redesign (chefkoch009) license change

--- README.md
+++ README.md
@@ -1,19 +1,85 @@
-![Logo](admin/eib_knx.png)
+![Logo](admin/knx.png)
 # ioBroker.knx
+=================
 
-This adapter provides an interface to a KNX bus system via eibd: http://www.auto.tuwien.ac.at/~mkoegler/index.php/eibd
+This adapter is a knx for the creation of an ioBroker adapter. You do not need it at least that you plan developing your own adapter.
 
-eibd in turn connects to a physical KNX interface like an IP Gateway or Router, USB KNX interfaces or direct TP
-interfaces (TPUARTS)
+It includes both code running within iobroker and as vis widget. If you only plan to create a vis widget then you should use the [iobroker.vis-eib-knx](https://github.com/ioBroker/ioBroker.vis-eib-knx) instead.
 
-ioBroker.knx talks to eibd using it's proprietary network protocol (not to be confused with EIBnet/IP) using the
-node-eibd module: https://github.com/andreek/node-eibd
+##Steps 
+1. download and unpack this packet from github ```https://github.com/ioBroker/ioBroker.eib-knx/archive/master.zip```
+  or clone git repository ```git clone https://github.com/ioBroker/ioBroker.eib-knx.git```
 
-NOTE: This adapter is currently INCOMPLETE and DYSFUNCTIONAL. Do NOT attempt to use it, it will turn you
-into a hamster.
+2. download required npm packets. Write in ioBroker.eib-knx directory:
 
-## ChangeLog
+  ```npm install```
+  
+3. set name of this eib-knx. Call
+  
+  ```grunt rename --name=mynewname --email=email@mail.com --author="Author Name"```
+  
+  *mynewname* must be **lower** case and with no spaces.
 
-### 0.0.1
-* (owagner) initial version
+  If grunt is not available, install grunt globally:
+  
+  ```npm install -g grunt-cli```
+ 
+4. rename directory from *ioBroker.eib-knx* (can be *ioBroker.eib-knx-master*) to *iobroker.mynewname*
 
+5. to use this eib-knx you should copy it into *.../iobroker/node_modules* directory and then create an instance for it with iobroker.admin
+
+6. create your adapter:
+
+  * you might want to start with knx.js (code running within iobroker) and admin/index.html (the adapter settings page).
+
+  * [Adapter-Development-Documentation](https://github.com/ioBroker/ioBroker/wiki/Adapter-Development-Documentation),
+  
+  * [Installation, setup and first steps with an ioBroker Development Environment](https://github.com/ioBroker/ioBroker/wiki/Installation,-setup-and-first-steps-with-an-ioBroker-Development-Environment)
+  
+  * [Write and debug vis widgets](https://github.com/ioBroker/ioBroker/wiki/How-to-debug-vis-and-to-write-own-widget-set)
+  
+  * files under the www folders are made available under http://&lt;iobrokerIP&gt;:8082/&lt;adapter-name&gt;/
+    * for this to work the iobroker.vis adapter has to be installed
+    * delete this folder if you do not plan to export any files this way
+    * call ```iobroker upload <adapter-name>``` after you change files in the www folder to get the new files uploaded to vis
+  * the widget folder contains an example of a vis widget
+    * you might want to start with *widget/<adapter-name>.html* and *widget/js/<adapter-name>.js*
+    * call ```iobroker visdebug <adapter-name>``` to enable debugging and upload widget to "vis". (This works only from V0.7.15 of js-controller)
+    * If you do not plan to export any widget then delete the whole widget folder and remove the ```"restartAdapters": ["vis"]``` statement from *io-package.json*
+    * After admin/index.html is changed you must execute ```iobroker upload mynewname``` to see changes in admin console. The same is valid for any files in *admin* and *www* directory  
+
+7. change version: edit package.json and then call ```grunt p``` in your adapter directory.
+  
+8. share it with the community
+
+## Changelog
+
+### 0.6.0 (2016-07-20)
+* (chefkoch009) redesign
+
+### 0.5.0
+  (vegetto) include vis widget
+
+#### 0.4.0
+* (bluefox) fix errors with grunt
+
+#### 0.2.0
+* (bluefox) initial release
+
+## License
+The CC-NC-BY License (CC-NC-BY)
+
+Copyright (c) 2016 K.Ringmann<info@punktnetzwerk.net>
+
+THE WORK IS PROVIDED UNDER THE TERMS OF THIS CREATIVE
+COMMONS PUBLIC LICENSE ("CCPL" OR "LICENSE"). THE WORK IS PROTECTED BY
+COPYRIGHT AND/OR OTHER APPLICABLE LAW. ANY USE OF THE WORK OTHER THAN AS
+AUTHORIZED UNDER THIS LICENSE OR COPYRIGHT LAW IS PROHIBITED.
+
+BY EXERCISING ANY RIGHTS TO THE WORK PROVIDED HERE, YOU ACCEPT AND AGREE
+TO BE BOUND BY THE TERMS OF THIS LICENSE. TO THE EXTENT THIS LICENSE MAY
+BE CONSIDERED TO BE A CONTRACT, THE LICENSOR GRANTS YOU THE RIGHTS
+CONTAINED HERE IN CONSIDERATION OF YOUR ACCEPTANCE OF SUCH TERMS AND
+CONDITIONS.
+
+Read full license text in [LICENSE](LICENSE)
--- knx.js
+++ knx.js
@@ -5,11 +5,13 @@
 var eibd = require('eibd');
 var parseString = require('xml2js').parseString;
 
+
 var eibdConnection;
-
+var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 // you have to require the adapter module and pass a options object
-var adapter = require(__dirname + '/../../lib/adapter.js')({
+var mapping = {};
 
+var adapter = utils.adapter({
     // name has to be set and has to be equal to adapters folder name and main file name excluding extension
     name:           'knx',
 
@@ -27,10 +29,10 @@ var adapter = require(__dirname + '/../../lib/adapter.js')({
             var ga = id.split('.')[2];
             adapter.log.info('setting state '+ga+' to '+state.val);
             var gad=eibd.str2addr(ga);
-            adapter.log.info(gad);
-            /* Todo: Guess DPT */
+            adapter.log.info('Gad : ' + gad);
+     //       / * Todo: Guess DPT */
             var tempCon=eibd.Connection();
-            tempCon.socketRemote({ host: adapter.config.eibdAddress, port: adapter.config.eibdPort },function(x){
+            tempCon.socketRemote({ host: adapter.config.gwip, port: adapter.config.gwipport },function(x){
                 tempCon.openTGroup(gad,1,function(err){
                     var data=new Array(2);
                     data[0]=0;
@@ -49,10 +51,10 @@ var adapter = require(__dirname + '/../../lib/adapter.js')({
         try {
             if (eibdConnection) {
                 /* Workaround against Parser not implementing end() - https://github.com/andreek/node-eibd/issues/7 */
-                eibdConnection.parser.end=function(){ /* Dummy */ };
+                if (eibdConnection.parser) eibdConnection.parser.end = function(){ /* Dummy */ };
                 eibdConnection.end();
             }
-            adapter.log.info('cleaned everything up...');
+            // adapter.log.info('cleaned everything up...');
         } finally {
             callback();
         }
@@ -67,40 +69,68 @@ var adapter = require(__dirname + '/../../lib/adapter.js')({
 
 });
 
-function main() {
+function parseXml(text, callback) {
+    parseString(text, function(err, result){
+        //Extract the value from the data element
+        callback(err, result ? result['GroupAddress-Export'] : null);
+    });
+}
 
+function main() {
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
-    adapter.log.info('Connecting to eibd ' + adapter.config.eibdAddress + ":" +adapter.config.eibdPort);
+    adapter.log.info('Connecting to eibd ' + adapter.config.gwip + ":" +adapter.config.gwipport);
 
-    // Parse the group address table
-    function parseGARange(gaRange)
-    {
-        for(var ix in gaRange) {
-            var gar=gaRange[ix];
-            if(gar.GroupRange)
-            {
-                parseGARange(gaRange[ix].GroupRange)
-            }
-            else if(gar.GroupAddress) {
-                for(var gaIX in gar.GroupAddress)
-                {
-                    var ga=gar.GroupAddress[gaIX].$;
-                    console.log(ga);
+    var gaTable = adapter.config.gaTable;
+
+    adapter.log.info(utils.controllerDir);
+
+    function parseGARange(gaRange, path) {
+        if (!gaRange) return adapter.log.error('Unknown XML format. No GroupRange found');
+
+        path = path || '';
+
+        // Main groups
+        for (var ix = 0; ix < gaRange.length; ix++) {
+            var gar = gaRange[ix];
+            if (gar.GroupRange) {
+                var locpath = path;
+                if (gar.$ && gar.$.Name) {
+                    locpath += (path ? '.' : '') + gaRange[ix].$.Name.replace(/\./g, '_').replace(/\s/g, '_');
+                }
+                parseGARange(gaRange[ix].GroupRange, locpath);
+            } else if (gar.GroupAddress) {
+                var locpath = path;
+                if (gar.$ && gar.$.Name) {
+                    locpath += (path ? '.' : '') + gar.$.Name.replace(/\./g, '_').replace(/\s/g, '_');
+                }
+                for (var gaIX = 0; gaIX < gar.GroupAddress.length; gaIX++) {
+                    var ga = gar.GroupAddress[gaIX].$;
+                    //adapter.log.info('ga : ' + ga);
                     // Heavy magic - enrich object if there, create otherwise
-                    var obj={_id: ga["Address"], type: "state", common: {name: ga["Name"] }, native: {} };
-                    console.log(obj)
-                    adapter.extendObject(obj._id,obj);
+                    var obj = {_id: (locpath ? locpath + '.' : '') + ga.Address.replace(/\//g, '_'), type: 'state', common: {name: ga.Name}, native: {address: ga.Address}};
+                    //adapter.log.info('obj : ' + obj);
+                    //obj.getElementsByName("Address")
+                    adapter.extendObject(obj._id, obj);
+                    mapping[ga.Address] = obj;
                 }
             }
         }
     }
 
-    parseString(adapter.config.gaTable, function (err, result) {
-        parseGARange(result["GroupAddress-Export"].GroupRange);
-    });
 
-    // Establish the eibd connection
+   // function parseGARange(gaRange) {
+   //     adapter.log.info('gaRange : ' + gaRange);
+   // }
+
+    /*parseString(adapter.config.gaTable, function (err, result) {
+        adapter.log.info('parseString : ' + result["GroupAddress-Export"]);
+        parseGARange(result["GroupAddress-Export"].GroupRange);
+        //adapter.log.info("GroupAddress-Export : " + result["GroupAddress-Export"].GroupRange);
+    });*/
+
+
+   // Establish the eibd connection
     function groupsocketlisten(opts, callback) {
         eibdConnection = eibd.Connection();
         eibdConnection.socketRemote(opts, function() {
@@ -108,23 +138,81 @@ function main() {
         });
     }
 
-    // and setup the message parser
-    groupsocketlisten({ host: adapter.config.eibdAddress, port: adapter.config.eibdPort }, function(parser) {
+    parseXml(adapter.config.gaTable, function (error, result) {
+        if (result) {
+            parseGARange(result.GroupRange);
+        }
 
-        parser.on('write', function(src, dest, dpt, val){
-            /* Message received to a GA */
-            adapter.log.info('Write from '+src+' to '+dest+': '+val+' ('+dpt+')');
-            adapter.setState(dest,{val: val, ack: true, from: src});
+        // and setup the message parser
+        groupsocketlisten({host: adapter.config.gwip, port: adapter.config.gwipport}, function (parser) {
+            parser.decoder.decode = function (len, data, callback) {
+
+                var err = null;
+                var type = 'DPT1';
+                var value = null;
+
+                // eis 1 / dpt 1.xxx
+                if(len === 8) {
+                    value = data-64;
+                    if(value > 1) {
+                        value = value-64;
+                    }
+                }
+
+                // eis 6 / dpt 5.xxx
+                // assumption
+                if(len === 9){
+                    type = 'DPT5';
+                    if(data.length === 1) {
+                        value = this.decodeDPT5(data);
+                    } else {
+                        err = new Error('Invalid data len for DPT5');
+                    }
+                }
+
+                // eis 5 / dpt 9.xxx
+                // assumption
+                if(len === 10) {
+                    type = 'DPT9';
+                    if(data.length === 2) {
+                        value = this.decodeDPT9(data);
+                    }
+                    else {
+                        err = new Error('Invalid data len for DPT9');
+                    }
+                }
+                if (len === 12) {
+                    type = 'DPT12';
+                    // float
+                    if(data.length === 4) {
+                        value = this.decodeDPT12(data);
+                    }
+                    else {
+                        err = new Error('Invalid data len for DPT9');
+                    }
+                }
+
+                if(callback) {
+                    callback(err, type, value);
+                }
+            };
+            parser.on('write', function(src, dest, dpt, val){
+                if (mapping[dest]) dest = mapping[dest].common.name;
+                /* Message received to a GA */
+                adapter.log.info('Write from ' + src + ' to ' + dest + ': ' + val + ' (' + dpt + ')');
+                adapter.setState(dest,{val: val, ack: true, from: src});
+            });
+
+            parser.on('response', function(src, dest, val) {
+                if (mapping[dest]) dest = mapping[dest].common.name;
+                adapter.log.info('Response from ' + src + ' to ' + dest + ': '+val);
+            });
+
+            parser.on('read', function(src, dest) {
+                if (mapping[dest]) dest = mapping[dest].common.name;
+                adapter.log.info('Read from ' + src + ' to ' + dest);
+            });
+
         });
-
-        parser.on('response', function(src, dest, val) {
-            adapter.log.info('Response from '+src+' to '+dest+': '+val);
-        });
-
-        parser.on('read', function(src, dest) {
-            adapter.log.info('Read from '+src+' to '+dest);
-        });
-
     });
-
 }
