cd0b1a9c48e757b930ec4a42df9ddd20908b2516
chefkoch009 <info@punktnetzwerk.net>
2016-08-11 23:35:28 +0200

    * ESF-Text import parser ready and working * extract DPT (in old EIS form)

--- knx.js
+++ knx.js
@@ -3,15 +3,17 @@
 "use strict";
 
 var eibd = require('eibd');
+
+var parseESFString = parseESFString;
 var parseString = require('xml2js').parseString;
 
+var esfBuf = require('text-encoding');
 
 var eibdConnection;
 var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 
 var mapping = {};
 var valtype ='';
-var esf = new FileReader;
 
 var adapter = utils.adapter({
     // name has to be set and has to be equal to adapters folder name and main file name excluding extension
@@ -160,11 +162,86 @@ var adapter = utils.adapter({
 });
 
 function parseXml(text, callback) {
-    parseString(text, function(err, result){
-        //Extract the value from the data element
-        callback(err, result ? result['GroupAddress-Export'] : null);
-    });
+        //adapter.log.info(' text : ' + text);
+        parseString(text, function (err, result) {
+            //Extract the value from the data element
+            adapter.log.info('function parseXML');
+            callback(err, result ? result['GroupAddress-Export'] : null);
+        });
 }
+
+function parseESF(text, callback) {
+    parseESFString(text, function(err, result) {
+        //Extract the value from the data element
+        adapter.log.info('function parseESF');
+        callback(err, result ? result[' '] : null);
+    });
+
+}
+
+function parseESFString(text) {
+
+    adapter.log.info('function parseESFString   ... here we go :-)');
+    adapter.log.info('text länge : ' + text.length);
+    // Regexp for esf-Line String
+    var re_ga = /\d*[/b]\d*[/b]\d*/;
+    var re_hgName = /^[A-Za-z0-9]*/;
+    var re_mgName = /\.(.*)\./;
+    var re_ugName = /\.\d{1,3}\/\d{1,3}\/\d{1,3}\s(.*)\sEIS|\.\d{1,3}\/\d{1,3}\/\d{1,3}\s(.*)\sUncertain/;
+    var re_dpType = /\((\d\W.*)\)/;
+    var match;
+    var mgName = '';
+    var ugName = '';
+
+    var lines = text.split('\n');
+    for (var line=0; line < lines.length; line++) {
+        // Matcher
+        // Maingroup Name
+        var tmp = lines[line];
+        var hgName = tmp.match(re_hgName);
+
+        //Middlegroup Name
+        match = re_mgName.exec(tmp);
+        if (match){
+            mgName = match[1];
+        }
+
+        match = re_ugName.exec(tmp);
+        if (match){
+            if (match[2]) {
+                ugName = match[2];
+            } else {
+                ugName = match[1];
+            }
+        }
+
+        //Groupaddress
+        var ga = tmp.match(re_ga);
+
+        // DPT
+        var dp_Type = tmp.match(re_dpType);
+        match = re_dpType.exec(tmp);
+        if (match){
+            dp_Type = match[1];
+            switch (dp_Type) {
+                case (dp_Type == '1 Bit') :
+                    dp_Type = 'DPT1'
+                    break;
+            }
+        }
+        valtype = dp_Type;
+        var obj = {
+            _id: (hgName ? hgName + '.' : '' ) + (mgName ? mgName + '.' : '') + ga,
+            type: 'state',
+            common: {name: ugName},
+            native: {address: ga}
+        };
+        adapter.extendObject(obj._id, obj);
+        mapping[ga] = obj;
+        adapter.log.info('split : GA: ' + ga + '   ==> HG: ' + hgName + '     ==> MG: ' + mgName + '    ==> UG: ' + ugName + '    ==> DPT:   ' + dp_Type);
+    }
+}
+
 
 function main() {
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
@@ -172,14 +249,18 @@ function main() {
     adapter.log.info('Connecting to eibd ' + adapter.config.gwip + ":" +adapter.config.gwipport);
 
     var gaTable = adapter.config.gaTable;
+    var esfString = adapter.config.esfFile;
+
 
     adapter.log.info(utils.controllerDir);
+    //adapter.log.info('esffile : ' + esfString);
+    //<adapter.log.info('Buf : ' + decoder.decode (esfBuf));
+
 
     function parseGARange(gaRange, path) {
+
         if (!gaRange) return adapter.log.error('Unknown XML format. No GroupRange found');
-
         path = path || '';
-
         // Main groups
         for (var ix = 0; ix < gaRange.length; ix++) {
             var gar = gaRange[ix];
@@ -196,24 +277,70 @@ function main() {
                 }
                 for (var gaIX = 0; gaIX < gar.GroupAddress.length; gaIX++) {
                     var ga = gar.GroupAddress[gaIX].$;
-                    var obj = {_id: (locpath ? locpath + '.' : '') + ga.Address.replace(/\//g, '_'), type: 'state', common: {name: ga.Name}, native: {address: ga.Address}};
+                    var obj = {
+                        _id: (locpath ? locpath + '.' : '') + ga.Address.replace(/\//g, '_'),
+                        type: 'state',
+                        common: {name: ga.Name},
+                        native: {address: ga.Address}
+                    };
                     adapter.extendObject(obj._id, obj);
                     mapping[ga.Address] = obj;
                 }
             }
         }
+
     }
 
 
    // Establish the eibd connection
     function groupsocketlisten(opts, callback) {
         eibdConnection = eibd.Connection();
-        eibdConnection.socketRemote(opts, function() {
-            eibdConnection.openGroupSocket(0, callback);
-        });
+        try {
+            eibdConnection.socketRemote(opts, function () {
+                eibdConnection.openGroupSocket(0, callback);
+            });
+            throw "connectionError";
+        }
+        catch (e) {
+            if (e == "connectionError")
+                adapter.log.error("Connection Error. I'm not able to write to LAN-KNX Gateway ! Check availability of " + adapter.config.gwip);
+        } finally {
+
+        }
     }
 
+    parseESF(adapter.config.esfFile, function (error, result) {
+        adapter.log.info('parseESF');
+        if (result){
+               parseESFString(result);
+        }
+        // and setup the message parser
+        groupsocketlisten({host: adapter.config.gwip, port: adapter.config.gwipport}, function (parser) {
+                adapter.log.info('ESF' + adapter.config.esfFile);
+        });
+
+        parser.on('write', function(src, dest, dpt, val){
+            //if (mapping[dest]) var mappedName = mapping[dest].common.name;
+            /* Message received to a GA */
+            //adapter.log.info('Write from ' + src + ' to ' + '(' + dest + ') ' + mappedName + ': ' + val + ' (' + dpt + ')');
+            valtype = dpt;
+            adapter.log.info('====>> ESF File : ' );
+            adapter.setState(mappedName + '.' + dest.replace(/\//g, '_') + '.'+ dpt,{val: val, ack: true, from: src});
+        });
+
+        parser.on('response', function(src, dest, val) {
+            if (mapping[dest]) var mappedName = mapping[dest].common.name;
+            adapter.log.info('Response from ' + src + ' to ' + '(' + dest + ') ' + mappedName + ': '+val);
+        });
+
+        parser.on('read', function(src, dest) {
+            if (mapping[dest]) var mappedName = mapping[dest].common.name;
+            adapter.log.info('Read from ' + src + ' to ' + '(' + dest + ') ' + mappedName);
+        });
+    });
+
     parseXml(adapter.config.gaTable, function (error, result) {
+        adapter.log.info('parseXml');
         if (result) {
             parseGARange(result.GroupRange);
         }
@@ -277,7 +404,7 @@ function main() {
                 /* Message received to a GA */
                 //adapter.log.info('Write from ' + src + ' to ' + '(' + dest + ') ' + mappedName + ': ' + val + ' (' + dpt + ')');
                 valtype = dpt;
-                adapter.log.info('====>> ESF File : ' + esf.Name);
+               // adapter.log.info('====>> ESF File : ' + esf.Name);
                 adapter.setState(mappedName + '.' + dest.replace(/\//g, '_'),{val: val, ack: true, from: src});
             });
 
@@ -293,4 +420,5 @@ function main() {
 
         });
     });
+
 }
